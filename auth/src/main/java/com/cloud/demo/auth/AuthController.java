1098bc473172dbbbb6fec25e6ef1f04588fe9063
inhyeok <ss112d86@gmail.com>
2022-05-08 05:24:08 +0900

    configserver 셋팅

--- auth/src/main/java/com/cloud/demo/auth/AuthController.java
+++ auth/src/main/java/com/cloud/demo/auth/AuthController.java
@@ -1,5 +1,6 @@
 package com.cloud.demo.auth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+  @Value("${contents.value}")
+  private String str;
+
   @GetMapping("")
   public String authGet() {
     return "hello auth";
+  }
+
+  @GetMapping("/config/info")
+  public String configInfo() {
+    return str;
   }
 }
